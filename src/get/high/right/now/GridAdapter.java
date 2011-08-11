1b71ca074700e0cf020c3a691c18455fe52e3562
r3dsm0k3 <ajithredsmoke@gmail.com>
2011-08-11 23:17:04 +0530

    added current branch

--- src/get/high/right/now/GridAdapter.java
+++ src/get/high/right/now/GridAdapter.java
@@ -1,18 +1,27 @@
 package get.high.right.now;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GridAdapter extends BaseAdapter {
-	private Context context;
-	private String[] texts = {"Whiskey","Brandy", "Rum", "Vodka" ,"Gin" , "Beer", "Wine"};
+	
+	private Context context=null;
+	
+	ViewHolder holder=null;
+	
+	LayoutInflater mInflater=null; 
+	
+	private String[] texts = {"FootBall","Cricket", "Carom Board", "Tennis" ,"Hockey" , "Volley Ball", "Ashes"};
 
 	public GridAdapter(Context context) {
 	    this.context = context;
+	    mInflater = LayoutInflater.from(context);
 	}
 
 	public int getCount() {
@@ -20,25 +29,33 @@ public class GridAdapter extends BaseAdapter {
 	}
 
 	public Object getItem(int position) {
-	    return null;
+	    return position;
 	}
 
 	public long getItemId(int position) {
-	    return 0;
+	    return position;
 	}
 
 	public View getView(int position, View convertView, ViewGroup parent) {
-	    TextView tv;
+	   
 	    if (convertView == null) {
-	        tv = new TextView(context);
-	        tv.setLayoutParams(new GridView.LayoutParams(85, 85));
+	    	holder = new ViewHolder();
+	        convertView = mInflater.inflate(R.layout.grid_item, null);
+	        holder.tv = (TextView) convertView.findViewById(R.id.grid_item_label);
+	        holder.logo_img =(ImageView)convertView.findViewById(R.id.grid_item_icon);
 	    }
-	    else {
-	        tv = (TextView) convertView;
-	    }
+	  
 
-	    tv.setText(texts[position]);
-	    return tv;
+	    holder.tv.setText(texts[position]);
+	    holder.logo_img.setImageResource(R.drawable.icon);
+	    return convertView;
 
+	}
+	
+	static class ViewHolder {
+		
+		TextView tv;
+		ImageView logo_img;
+		
 	}
 }
